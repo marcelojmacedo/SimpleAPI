496d5d48adb0b4cf6221c36124b1f482f4db7e9a
Marcelo Jose Macedo <marcelomacedo1980@hotmail.com>
2019-06-14 23:01:13 +0800

    Rollback

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPISln.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Joao";
+            return "Marcelo";
         }
 
         // POST api/values
